23017f8c3b59e8c936ca441648a22ae3fbe7ea91
Michael Flynn <mflynn37211@yahoo.com>
2014-08-24 18:16:52 -0400

    initial commit

---
+++ leaderboard.rb
@@ -0,0 +1,62 @@
+require 'csv'
+require 'pry'
+require 'sinatra'
+require 'sinatra/reloader'
+
+
+##########Methods############
+
+
+def team_info(team_name)
+  teams = []
+  CSV.foreach(team_name, headers: true, header_converters: :symbol, converters: :integer) do |row|
+    teams << row.to_hash
+  end
+  teams
+end
+
+def sort_teams(info)
+  info.sort_by {|key, value| value}
+end
+
+
+
+
+
+get '/leaderboard' do
+  @teams = team_info("teams.csv")
+  team_win = Hash.new(0)
+  team_loss = Hash.new(0)
+  won = []
+  lost = []
+
+
+  @teams.each do |team|
+    if team[:away_score] > team[:home_score]
+      won << team[:away_team]
+    else team[:away_score] < team[:home_score]
+        won << team[:home_team]
+    end
+  end
+
+  won.each do |team_wins|
+    team_win[team_wins] += 1
+  end
+
+
+  @teams.each do |team|
+    if team[:away_score] > team[:home_score]
+      lost << team[:home_team]
+    else team[:away_score] < team[:home_score]
+      lost << team[:away_team]
+    end
+  end
+
+  lost.each do |teamloss|
+    team_loss[teamloss] += 1
+  end
+
+  @winnners = sort_teams(team_win).reverse
+  @losses = sort_teams(team_loss).reverse
+  erb :leaderboard
+end
